87544c070a009bcf8ab3a1b9354095151a1a547c
Koosha <koosha.g@gmail.com>
2016-06-17 14:41:30 -0600

    Init project. Extract tweets and sentiment analysis

---
+++ twitter_sent_analysis.py
@@ -0,0 +1,42 @@
+from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, credsfromfile
+
+import urllib
+
+
+
+def get_sentiment(lst):
+    twts = lst
+    for twt in twts:
+        # sentiment analysis
+        data = urllib.urlencode({"text": twt})
+        u = urllib.urlopen("http://text-processing.com/api/sentiment/", data)
+        the_page = u.read()
+        print the_page, twt
+
+
+
+if __name__ == '__main__':
+
+    oauth = credsfromfile()
+    # search a topic
+    # tw = Twitter()
+    # tw.tweets(keywords='edmonton arena', stream=False, limit=10)
+
+    client = Query(**oauth)
+    tweets = client.search_tweets(keywords='covenant care', limit=10)
+    tweet = next(tweets)
+
+    tweets_txt=[]
+    for tweet in tweets:
+        tweets_txt.append(tweet['text'].encode('utf-8'))
+        print("- "+tweet['text'])
+
+
+    # sentiment analysis
+    get_sentiment(tweets_txt)
+    
+    print ("EOP")
+
+
+
+
